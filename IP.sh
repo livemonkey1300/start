77ef6843fb3a952a85380ee2bd3d8d9f7676acd8
Adam CorriveAU <adam@dnsnetworks.ca>
2018-09-05 13:11:11 -0400

    Replace file
    Adding logic to replace files

--- IP.sh
+++ IP.sh
@@ -10,3 +10,6 @@ do
 var=$(openssl rand -base64 32 | sed 's/[^a-zA-Z0-9]//g' )
 echo "$line , $var"
 done < PasswordFile
+
+sed 's/[A-Za-z][A-Za-z]*$/replace/' file.txt
+ls -1 /etc/sysconfig/network-scripts/ifcfg-*
