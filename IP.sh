e9b70d169fff9cb3d440375b2d49be8302c45087
Adam CorriveAU <adam@dnsnetworks.ca>
2018-09-05 13:48:48 -0400

    Add time
    Add time for Password

--- IP.sh
+++ IP.sh
@@ -5,10 +5,15 @@
 # Generate a bunch of hash #
 ############################
 
+PasswordDate=$(date +%F-%T)
+PasswordFile="./Data/PasswordFile-$PasswordDate"
+touch -p "$PasswordFile"
+
 while read line 
 do 
 var=$(openssl rand -base64 32 | sed 's/[^a-zA-Z0-9]//g' )
 echo "$line , $var"
+echo "export $line=$var" >> "$PasswordFile"
 done < PasswordFile
 
 #sed 's/[A-Za-z][A-Za-z]*$/replace/' file.txt
