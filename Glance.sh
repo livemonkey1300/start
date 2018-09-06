7850f2a3ef687c7b5ccd83ca7b5cd3613938295b
Adam corriveau <adam@dnsnetworks.ca>
2018-09-06 12:32:52 -0400

    test

--- Glance.sh
+++ Glance.sh
@@ -15,7 +15,7 @@ yum -y install openstack-glance
 
 sed -i "/^\[database\]$/a connection = mysql+pymysql://glance:$GLANCE_DBPASS@controller/glance"  /etc/glance/glance-api.conf
 
-keystone_authtoken='Glance/keystone_authtoken'
+keystone_authtoken='./SysData/Glance/keystone_authtoken'
 while read line 
 do 
 sed -i "/^\[keystone_authtoken\]$/a $line"  /etc/glance/glance-api.conf
